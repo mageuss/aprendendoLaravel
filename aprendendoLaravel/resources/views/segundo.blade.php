975f0b307c6e69b4b41036261b2e4823d9c7258f
mageuss <mateus.santos@aedb.br>
2024-04-30 14:30:15 -0300

    segundo dia estudando um pouco de lavarel

---
+++ aprendendoLaravel/resources/views/segundo.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Segunda página')
+
+@section('content')
+    
+<h1>Sou a Segunda pagina criada, eba!</h1>
+
+@endsection
--- aprendendoLaravel/routes/web.php
+++ aprendendoLaravel/routes/web.php
@@ -6,7 +6,11 @@ Route::get('/', function () {
 
     $nome = "Mateus";
     $idade = 21;
+    $array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-    return view('welcome', ['nome' => $nome, 'idade'=> $idade]);
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'array' => $array]);
 });
 
+Route::get('/segundo', function (){
+    return view('segundo');
+});
