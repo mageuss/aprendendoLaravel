2afea5ab0548701022707ee8f49696d1bbde5008
mageuss <mateus.santos@aedb.br>
2024-05-08 13:32:17 -0300

    organizacao, estilizacao e imagem nova

--- aprendendoLaravel/resources/views/welcome.blade.php
+++ aprendendoLaravel/resources/views/welcome.blade.php
@@ -6,26 +6,29 @@
     
 
 <h1> Hello World </h1>
+<a href="/segundo">segunda pagina</a>
+
 @if ($nome == 'Mateus')
     <p>o {{ $nome }} tem {{ $idade }} anos</p>
 @else
     <p>oii</p>
 @endif
 
-@for ($i = 0; $i < count($array); $i++)
+{{-- @for ($i = 0; $i < count($array); $i++)
     @if ($array[$i] % 2 == 0)
         <p> O numero {{ $array[$i] }} é par </p>
     @endif
     <p>{{ $array[$i] }}</p>
-@endfor
+@endfor --}}
 
 {{-- tem como criar blocos de php no blade --}}
-@php
+{{-- @php
     $nomes = ['lucas', 'mateus', 'joão', 'luis'];
 @endphp
 @foreach ($nomes as $item)
     {{ $item }},
-@endforeach
+@endforeach --}}
 <!--comentario em html aparece no inspecionar elemento, os outros não-->
 {{-- assim q faz comentário no blade --}}
+{{-- @endsection --}}
 @endsection
